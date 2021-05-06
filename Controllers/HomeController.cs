3bb8efbea9deeadac0cb561e4aed91e4c1cbf761
edratabaku <edratabaku@users.noreply.github.com>
2021-05-06 19:34:51 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+				{
+            return View();
+				}
+
+        public ActionResult Login()
+				{
+            return View();
+				}
+    }
+}
